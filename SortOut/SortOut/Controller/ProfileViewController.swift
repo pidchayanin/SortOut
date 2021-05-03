51671ec717b163b29e8782e657d667fc751386c9
Chutipattanap <33032841+Chutipattanap@users.noreply.github.com>
2021-05-03 15:01:58 +0700

    profile page
    datepicker

--- SortOut/SortOut/Controller/ProfileViewController.swift
+++ SortOut/SortOut/Controller/ProfileViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ProfileViewController.swift
 //  SortOut
@@ -7,19 +8,20 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:ProfileTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! ProfileTableViewCell
+class ProfileViewController: UIViewController {
+    
+    //func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //    let cell:ProfileTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! ProfileTableViewCell
         
-        cell.settingLabel.text = options[indexPath.row]
-        return cell
-    }
+    //    cell.settingLabel.text = options[indexPath.row]
+    //    return cell
+   // }
     
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // your number of cells here
-        return self.options.count
-    }
+   //     return self.options.count
+   // }
 
     /*
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
@@ -32,16 +34,61 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     
 
     @IBOutlet weak var PlayerNameLabel: UILabel!
-    @IBOutlet weak var tableView: UITableView!
+    //@IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var notificationTime: UITextField!
+    
+    @IBOutlet weak var notificationDate: UITextField!
+    
+    //**START time and date picker
+    let datePicker = UIDatePicker()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.PlayerNameLabel.text = "Player" //add player name from apple game center
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
+     
+        createDatePicker()
+        
+        //  self.tableView.delegate = self
+     //   self.tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    func createDatePicker() {
+        
+        notificationDate.textAlignment = .center
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // bar button
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        
+        // assign toolbar to the keyboard
+        notificationDate.inputAccessoryView = toolbar
+        
+        // assign date picker to the textfield
+        notificationDate.inputView = datePicker
+        
+        // date picker mode: change it to just date
+        datePicker.datePickerMode = .date
+    }
+    
+    @objc func donePressed() {
+        // create a formatter
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none // do not want to display time
+        
+        
+        //notificationDate.text = "\(datePicker.date)"
+        notificationDate.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+        
+        print(notificationDate.text!)
+    }
     
     /*
     // MARK: - Navigation
