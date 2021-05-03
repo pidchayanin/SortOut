aea5045de4b9d247559c818c80c7b541048759f6
Chutipattanap <33032841+Chutipattanap@users.noreply.github.com>
2021-05-03 23:45:30 +0700

    popup time and date
    update

---
+++ SortOut/SortOut/View/DatePopUpViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DatePopUpViewController.swift
+//  SortOut
+//
+//  Created by Pidchayanin Chutipattana on 3/5/2564 BE.
+//
+
+import UIKit
+
+class DatePopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SortOut/SortOut/View/TimePopUpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TimePopUpViewController.swift
+//  SortOut
+//
+//  Created by Pidchayanin Chutipattana on 3/5/2564 BE.
+//
+
+import UIKit
+
+class TimePopUpViewController: UIViewController {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timePicker: UIDatePicker!
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doneBtn_TouchUpinside(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
