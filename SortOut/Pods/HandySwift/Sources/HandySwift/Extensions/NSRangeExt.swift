459980c4392414aed165ea025e1f0a5a06a42d19
Chutipattanap <33032841+Chutipattanap@users.noreply.github.com>
2021-05-06 22:57:54 +0700

    try to config LFS

---
+++ SortOut/Pods/HandySwift/Sources/HandySwift/Extensions/NSRangeExt.swift
@@ -0,0 +1,12 @@
+// Copyright © 2019 Flinesoft. All rights reserved.
+
+import Foundation
+
+extension NSRange {
+    /// Initializes an NSRange from a Swift String.Range when the String is provided.
+    public init(_ range: Range<String.Index>, in string: String) {
+        self.init()
+        self.location = string.utf16.distance(from: string.startIndex, to: range.lowerBound)
+        self.length = string.utf16.distance(from: range.lowerBound, to: range.upperBound)
+    }
+}
