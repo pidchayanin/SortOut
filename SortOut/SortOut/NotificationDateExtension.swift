b188812bb2f423b5b129585e1acdd0dbfa01891b
bleach015 <poppula_12@hotmail.com>
2021-05-09 14:23:54 +0700

    hot fix

---
+++ SortOut/SortOut/NotificationDateExtension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationDateExtension.swift
+//  SortOut
+//
+//  Created by Pidchayanin Chutipattana on 9/5/2564 BE.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let saveDateDay = Notification.Name(rawValue: "saveDateDay")
+}
+
